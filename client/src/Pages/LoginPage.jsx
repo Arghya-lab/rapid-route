df84f75fe7780fb9daa7905d6c8618a789971b48
Arghya Maity <maityarghya0@gmail.com>
2023-09-28 23:57:34 +0530

    configure for daisyUi & added login page

---
+++ client/src/Pages/LoginPage.jsx
@@ -0,0 +1,36 @@
+function LoginPage() {
+  return (
+    <>
+      <form className="flex flex-col items-center mt-12 mb-6 space-y-4">
+      <p className="mb-4 text-3xl font-semibold text-neutral">Login</p>
+        <div className="w-full max-w-xl space-y-1">
+          <p className="ml-3 font-semibold text-neutral">Name</p>
+          <input
+            type="text"
+            placeholder="Name"
+            className="input input-bordered input-primary w-full"
+          />
+        </div>
+        <div className="w-full max-w-xl space-y-1">
+          <p className="ml-3 font-semibold text-neutral">Email</p>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input input-bordered input-primary w-full"
+          />
+        </div>
+        <div className="w-full max-w-xl space-y-1">
+          <p className="ml-3 font-semibold text-neutral">Password</p>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered input-primary w-full"
+          />
+        </div>
+        <button className="btn btn-wide btn-neutral">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginPage;
